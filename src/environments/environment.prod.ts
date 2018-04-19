f4c362a95d19e53e6227fdd2bc7f84c8c0ba5f9a
JM_B <jmbanchero@gmail.com>
2018-04-19 09:47:10 +0100

    prod 1 fail

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDJzlwcQRKz0kwKyXRB44Lm-OMP805KSrQ',
+    authDomain: 'smart-home-78b50.firebaseapp.com',
+    databaseURL: 'https://smart-home-78b50.firebaseio.com',
+    projectId: 'smart-home-78b50',
+    storageBucket: 'smart-home-78b50.appspot.com',
+    messagingSenderId: '1030652514308'
+  }
 };
