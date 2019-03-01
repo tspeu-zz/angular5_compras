fbad95f7c11c135fbc1cec57024a5d7ee69c7a48
JM_B <jmbanchero@gmail.com>
2019-03-01 08:57:49 +0000

    fix delete documentos

--- src/app/servicios/load.service.ts
+++ src/app/servicios/load.service.ts
@@ -3,6 +3,7 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase';
 import { Archivo } from '../uploads/file.modal';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class LoadfileService {
@@ -14,7 +15,10 @@ uploadsRef: AngularFireList<Archivo>;
 
 uploads: Observable<any[]>;
 
-constructor(private angularFireDatabase: AngularFireDatabase ) { }
+constructor(private angularFireDatabase: AngularFireDatabase) { 
+
+              let Uuid =  uuid(); //<-- use it
+            }
 
 
   getUploads() {
@@ -26,6 +30,7 @@ constructor(private angularFireDatabase: AngularFireDatabase ) { }
   pushUpload(archivo: Archivo) {
     const storageRef = firebase.storage().ref();
     const uploadTask = storageRef.child(`${this.basePath}/${archivo.file.name}`).put(archivo.file);
+    console.log("uploadTask-->", uploadTask);
 
     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
       (snapshot: any) => {
@@ -45,30 +50,52 @@ constructor(private angularFireDatabase: AngularFireDatabase ) { }
     }, () => {
         archivo.url = uploadTask.snapshot.downloadURL;
         archivo.name = archivo.file.name;
+        console.log("--archivo-> ", archivo);
         this.saveFiledata(archivo);
+        let _key = archivo.$key;
+        console.log('_KEY', _key);
     });
   }
 
   private saveFiledata (fichero: Archivo) {
+    console.log('<--- el fichero saveFiledata -->', fichero);
     this.angularFireDatabase.list(`${this.basePath}/`).push(fichero);
   }
 
   deleteUpload(fichero: Archivo) {
-    this.deleteFileData(fichero.$key)
+    console.log('<--- el ficheroa  deleteUpload -->', fichero);
+    let _key = fichero.$key;
+    console.log('_KEY deleteUpload --> ', _key);
+    // this.deleteFileData(fichero.$key)
+    this.deleteFileStorage(fichero.name)
     .then( () => {
-      this.deleteFileStorage(fichero.name);
+      console.log('deleteFileData KEY', fichero);
+      // this.getUploads();
     }).catch( err => console.log('err en delete', err));
   }
 
   // borra los metadatos del fichero
   private deleteFileData(key: string) {
-    return this.angularFireDatabase.list(`${this.basePath}/`).remove(key);
+    console.log('deleteFileData KEY', key);
+    console.log(`ESOTE ES--> ${this.basePath}/key`);
+    // return this.angularFireDatabase.list(`${this.basePath}/`).remove(key);
+    //return this.angularFireDatabase.list(`${this.basePath}/`).remove(key);
+    return this.angularFireDatabase.object(`${this.basePath}/key`).remove();
+    //list(`${this.basePath}/`).remove(key);
   }
+
+/*  TODO  deleteItem(key: string) {
+    this.itemsRef.remove(key);
+  }
+*/
+
 
   // borra el fichero del storage
   private deleteFileStorage(name: string) {
+    console.log('name deleteFileStorage --> ', name);
+    console.log(`ESTP ES --> BASEPATH  -> ${this.basePath}/${name}`);
     const storageRef = firebase.storage().ref();
-    storageRef.child(`${this.basePath}/${name}`).delete();
+    return storageRef.child(`${this.basePath}/${name}`).delete();
   }
 
 }
